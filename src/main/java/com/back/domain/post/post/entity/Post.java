0420774eb2353552e8701078934521131aa15fab
BeomBeom2 <whwjdqja20@naver.com>
2026-05-04 15:05:25 +0900

    19강

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,18 +1,21 @@
 package com.back.domain.post.post.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@RequiredArgsConstructor
+@ToString
 public class Post {
     @Id //PK
     @GeneratedValue(strategy = GenerationType.IDENTITY) //AUTO_INCREMENT
     private int id; // INT
-    private String title; //VARCHAR(255) 이걸 JPA 가 해준다.
+    private final String title; //VARCHAR(255) 이걸 JPA 가 해준다.
+    @Column(columnDefinition = "TEXT")
+    private final String content;
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -7,6 +7,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Optional;
+
 @Configuration //빈, 내부에 @Bean 메서드 가질 수 있음
 public class BaseInitData {
     @Autowired
@@ -14,23 +16,23 @@ public class BaseInitData {
 
     @Bean //메서드는 스프링 부트가 시작할 때 자동으로 실행된다.
     ApplicationRunner baseInitDataApplicationRunner() {
+
         return args -> {
-            if(postRepository.count() > 0 ) return;
-
-            Post post1 = new Post();
-            post1.setTitle("제목 1");
-
-            postRepository.save(post1);
-
-
-            Post post2 = new Post();
-            post2.setTitle("제목 2");
-
-            postRepository.save(post2);
-
-            postRepository.count(); //SELECT COUNT(*) FROM post;
-
-            System.out.println("기본 데이터가 초기화되었습니다.");
+            work1();
+            work2();
         };
+    }
+
+    void work1() {
+        if(postRepository.count() > 0 ) return;
+
+        Post post1 = postRepository.save(new Post("제목 1", "내용 1"));
+        Post post2 = postRepository.save(new Post("제목 2", "내용 2"));
+
+        System.out.println("기본 데이터가 초기화되었습니다.");
+    }
+    void work2() {
+        Optional<Post> opPost1 = postRepository.findById(1);
+        // SELECT * FROM post WHERE id = 1;
     }
 }
