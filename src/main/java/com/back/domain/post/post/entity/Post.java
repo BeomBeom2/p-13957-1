ecb2937a926ebc03401ffa2446fccdcc1e375289
BeomBeom2 <whwjdqja20@naver.com>
2026-05-04 15:32:57 +0900

    21강-생성자를 편하게 만들기 위해 final 제거

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,26 +1,23 @@
 package com.back.domain.post.post.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
-@RequiredArgsConstructor
 @ToString
+@NoArgsConstructor
 public class Post {
     @Id //PK
     @GeneratedValue(strategy = GenerationType.IDENTITY) //AUTO_INCREMENT
     private int id; // INT
-    private final String title; //VARCHAR(255) 이걸 JPA 가 해준다.
+    private String title; //VARCHAR(255) 이걸 JPA 가 해준다.
     @Column(columnDefinition = "TEXT")
-    private final String content;
+    private String content;
 
-    public Post() {
-        this.title = "";
-        this.content = "";
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
