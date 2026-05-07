1900beeadd2190ba7953434c677af8692aa02ab5
BeomBeom2 <whwjdqja20@naver.com>
2026-05-07 10:35:09 +0900

    53강 멤버 관련 로직 구축

---
+++ src/main/java/com/back/domain/member/member/entity/Member.java
@@ -0,0 +1,29 @@
+package com.back.domain.member.member.entity;
+
+import com.back.global.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Member extends BaseEntity {
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String nickname;
+
+    public Member(String username, String password, String nickname) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
